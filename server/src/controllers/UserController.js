8768ecdbe47ecfc9a4b36e87eb9bc1d463d87d6e
lucas <lsantana1234@hotmail.com>
2023-06-02 22:40:36 -0300

    Add AppError para o tratamendo de exceçoes

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -1,7 +1,13 @@
+const AppError = require("../utils/AppError")
+
 class UserController {
   create(request, response) {
     const { name, email, password } = request.body
   
+    if(!name ){
+      throw new AppError("Name is required")
+    }
+
     response.json({name, email, password})
   }
 }
